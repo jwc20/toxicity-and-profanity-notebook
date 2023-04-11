afb87425efe024dd4822eb5671f22f22a0b25d8a
jwc20 <choij2015@gmail.com>
2023-04-10 17:12:23 -0700

    Create script to store dataset to ipfs node.

---
+++ ipfs_text.py
@@ -0,0 +1,16 @@
+import ipfshttpclient
+
+list_of_files = [
+    "social_media_toxicity_dataset.json",
+    "profanity_dataset.json",
+    "twitter_hate_speech_dataset.json",
+]
+
+with ipfshttpclient.connect("/ip4/127.0.0.1/tcp/5002") as client:
+    for file in list_of_files:
+        hash = client.add(file)
+        print(hash)
+
+        # write to txt file
+        with open("ipfs_hashes.txt", "a") as f:
+            f.write(hash["Hash"])
